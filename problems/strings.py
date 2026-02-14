b6ef0b8eaa7eb6cc6a04c5fdccd43b266dcbd2d2
Zinhle Hlongwane <zinhlehlongwani56@gmail.com>
2026-02-14 11:03:38 +0200

    Add strings.py code

---
+++ problems/strings.py
@@ -0,0 +1,8 @@
+def hollow_box(width: int, height: int) -> str:
+    pass
+
+def format_report(name: str, score: int, total: int) -> str:
+    pass
+
+def snake_to_camel(text: str) -> str:
+    pass
